cfead3ab06ee14c48312f9d0528a5943a75494be
Grasano <emilien.casano@gmail.com>
2016-02-20 01:24:54 +0100

    debut synthese vocale sur les trames + sous titres

---
+++ assets/js/synthese.js
@@ -0,0 +1,71 @@
+// TRAME 1
+//[Mathéo] Nous n’avons rien à faire aujourd’hui
+//[Emma] Nous allons rendre visite à l’oncle Ernest, il a toujours de super histoires !
+
+window.onload = function () {
+  // TODO si pas de speechsynthesis, on evite de l'utiliser, juste avec les sous titres
+  if (!('speechSynthesis' in window)) {
+    alert("You don't have speechSynthesis");
+  }
+}
+
+var trame1 = [
+  {
+    'speaker': "matheo",
+    'dialog': "Nous n’avons rien à faire aujourd’hui"
+  },
+  {
+    'speaker': "emma",
+    'dialog': "Nous allons rendre visite à l’oncle Ernest, il a toujours de super histoires !"
+  }
+];
+
+var trame2 = [
+  {
+    'speaker': "test",
+    'dialog': "dialog"
+  }
+]
+
+var trames = [
+  trame1,
+  trame2
+]
+
+
+function speakTrame(id, step, utterance) {
+  var trameCrt = trames[id];
+  console.log('TrameCrt : ' + trameCrt);
+
+  var nbtext = trameCrt.length;
+  console.info('Taille trameCrt : ' + nbtext);
+
+  // si non dernier dialogue
+  if (step < nbtext) {
+    // on recupere le dialogue et la speaker
+    var quiParle = trameCrt[step].speaker;
+    var dial = trameCrt[step].dialog;
+    // on modifie l'utterance pour la voix
+    utterance.text = dial;
+
+    // au debut, on change les sous titre
+    utterance.onstart = function (event) {
+      majSubtitles(dial, quiParle);
+    }
+
+    // a la fin de ce dialogue, on passe au suivant
+    utterance.onend = function (event) {
+      console.info('DIAL FINI ' + step);
+      step++;
+      speakTrame(id, step, utterance);
+    }
+
+    // on le speak
+    window.speechSynthesis.speak(utterance);
+  } else {
+    // sinon on peut passer à la trame suivante
+    console.info('NEXT TRAME');
+  }
+
+
+}
